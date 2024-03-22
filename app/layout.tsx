89e9578d47b2b02522f58be679bbd7eb58556ddf
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-03-22 16:45:46 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Complexity",
   description: "Complexity App",
-  icons: "/favicon.png",
+  icons: "/icon.png",
 };
 
 export default function RootLayout({
