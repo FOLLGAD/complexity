ed5fd9674d4456266668f45b9788bba884b7a4b8
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-03-22 16:48:30 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="favicon" href="/icon.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   );
