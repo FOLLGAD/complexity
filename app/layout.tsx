a8c8702aa5c25c89ad0e852853275c6be2e8856a
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-03-24 20:20:15 +0100

    add ph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { PHProvider } from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <link rel="favicon" href="/icon.png" sizes="any" />
-      <body className={inter.className}>{children}</body>
+      {/* posthog: */}
+      <PHProvider>
+        <body className={inter.className}>{children}</body>
+      </PHProvider>
     </html>
   );
 }
