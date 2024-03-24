a1237bdf5e165b4e5eb4a79fcbe00c7918fb2f08
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-03-24 21:21:07 +0100

    add og

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,19 @@ export const metadata: Metadata = {
   title: "Complexity",
   description: "Complexity App",
   icons: "/icon.png",
+  openGraph: {
+    title: "Complexity",
+    description: "Complexity App",
+    url: "https://complexity.emil.zip",
+    images: [
+      {
+        url: "https://complexity.emil.zip/og-image.png",
+        width: 1116,
+        height: 640,
+        alt: "Complexity",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +32,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <link rel="favicon" href="/icon.png" sizes="any" />
-      {/* posthog: */}
       <PHProvider>
         <body className={inter.className}>{children}</body>
       </PHProvider>
