87dd6004aefc8a80d5eb436f127e14aaa70ee3f6
Hamed Mohammadpour <HamedMP@users.noreply.github.com>
2024-04-04 23:14:14 +0200

    Linting + small font updates to make the title bolder

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="flex h-full w-full flex-col items-center pt-16">
       <div className="mb-2 flex flex-col items-center justify-between">
-        <div className="mx-auto mb-4 h-16 w-16 items-center justify-center align-middle group">
+        <div className="group mx-auto mb-4 h-16 w-16 items-center justify-center align-middle">
           <Logo />
         </div>
         <h1 className="text-gradient mb-1 mt-2 text-4xl font-medium tracking-tight">
@@ -15,15 +15,15 @@ export default function About() {
         The world's knowledge at your fingertips
       </p>
 
-      <div className="w-full max-w-xs pt-12 md:max-w-md md:pt-10 lg:max-w-xl">
-        <h2 className="text-2xl">About</h2>
-        <p className="mt-4 text-lg">
-          <span className="text-gradient">complexity</span> is a search engine
-          that uses AI to answer questions. It is built on top of the Cohere AI
-          platform and is designed to provide a fast and efficient way to find
-          answers to your questions.
+      <div className="w-full max-w-xs pt-8 md:max-w-md md:pt-10 lg:max-w-xl">
+        <h2 className="text-2xl font-bold">About</h2>
+        <p className="mt-4 text-lg font-light">
+          <span className="text-gradient font-medium">complexity</span> is a
+          search engine that uses AI to answer questions. It is built on top of
+          the Cohere AI platform and is designed to provide a fast and efficient
+          way to find answers to your questions.
         </p>
-        <h3 className="mt-4 text-xl">Created by</h3>
+        <h3 className="mt-6 text-xl font-medium">Created by</h3>
         <ul className="mt-4 list-inside list-disc marker:text-gray-400">
           <li className="mt-2">
             <a
