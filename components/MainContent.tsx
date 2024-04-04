301f0e002ef5848b748322c0037c8c931b5c6a9e
Hamed Mohammadpour <HamedMP@users.noreply.github.com>
2024-04-04 12:00:11 +0200

    Update the MainContent component to the new gray/orange design

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -81,60 +81,65 @@ const Session: FC = ({}) => {
 
 const Start = () => {
   const { ask, loading } = useComplexity();
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
   return (
     <div
       className={
-        "w-full h-full transition-all duration-100 ease-in-out flex flex-col items-center justify-center absolute top-0 bottom-0 "
+        'w-full transition-all duration-100 ease-in-out flex flex-col items-center justify-center absolute top-0 bottom-0 overflow-y-auto selection:bg-orange-200 selection:text-orange-600'
       }
     >
-      <div className="flex flex-col items-center justify-between mb-2">
-        <div className="w-24 h-24 mb-4">
-          <Logo />
-        </div>
-        <h1 className="text-3xl tracking-widest mb-2 mt-2">Complexity</h1>
+      <div className='flex flex-col items-center justify-between mb-2'>
+        <div className='w-24 h-24 mb-4'>{/* <Logo /> */}</div>
+        <h1 className='text-4xl font-medium tracking-tight mb-1 mt-2 text-gradient'>
+          complexity
+        </h1>
       </div>
-      <p className="mb-12">The world's knowledge at your fingertips</p>
+      <p className='mb-16 text-lg font-normal text-gray-300 animate-fade-in [--animation-delay:400ms]'>
+        The world's knowledge at your fingertips
+      </p>
       <form
-        className="w-full flex flex-col items-center gap-4 px-2"
+        className='w-full flex flex-col items-center gap-4 px-4'
         onSubmit={(e) => {
           e.preventDefault();
           ask(input, true);
-          setInput("");
+          setInput('');
         }}
       >
-        <div className="relative w-full max-w-lg">
+        <div className='relative w-full max-w-lg'>
           <Input
-            className="text-md p-4 py-6 text-gray-300 focus:text-primary focus:bg-primary/10 border focus:border-primary/20 rounded-lg"
-            placeholder="Ask anything..."
+            className='text-md p-4 pl-6 py-6 text-orange-50 focus:text-primary focus:bg-primary/5 border border-orange-50/10 focus:border-orange-100 rounded-full placeholder:text-gray-400 placeholder:font-thin'
+            placeholder='Ask anything...'
             onChange={(e) => setInput(e.target.value)}
             value={input}
             disabled={loading}
           />
-          <div className="absolute right-0 top-0 bottom-0 h-full flex items-center justify-center">
+          <div className='absolute right-0 top-0 bottom-0 h-full flex items-center justify-center'>
             <Button
-              variant="outline"
-              className="w-10 h-10 p-0 mr-1"
-              type="submit"
-              disabled={loading}
+              variant='outline'
+              className={cn(
+                'w-8 h-8 p-0 mr-2 rounded-full',
+                input ? 'bg-orange-400' : 'bg-gray-800'
+              )}
+              type='submit'
+              disabled={loading || !input}
             >
               {loading ? (
-                <LoaderCircle className="animate-spin" />
+                <LoaderCircle className='animate-spin' />
               ) : (
-                <ArrowRight />
+                <ArrowRight className='w-4 h-4' />
               )}
             </Button>
           </div>
         </div>
-        <h3 className="text-xs text-gray-400 font-bold mt-8 uppercase">
+        <h3 className='text-xs text-gray-200/90 font-bold mt-8 uppercase'>
           Trending
         </h3>
-        <div className="text-center flex flex-col gap-2 items-center">
+        <div className='text-center flex flex-col gap-2 items-center'>
           {examples.map((example) => (
             <Card
               key={example}
-              className="text-sm w-auto cursor-pointer mt-0 p-1 px-2 text-gray-300 hover:text-primary hover:bg-primary/10 border hover:border-primary/20 rounded-lg"
+              className='text-sm w-auto cursor-pointer mt-0 p-2 px-3 text-gray-300 hover:text-primary hover:bg-primary/10 border hover:border-orange-300/20 rounded-xl bg-[#202222]'
               onClick={() => {
                 if (loading) return;
                 setInput(example);
