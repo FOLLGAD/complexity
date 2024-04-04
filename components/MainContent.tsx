9a406aefaa4fee33cb9cb23bc9f993a4016b82e9
Hamed Mohammadpour <HamedMP@users.noreply.github.com>
2024-04-04 13:00:07 +0200

    Clean up unused code, comments, etc

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,23 +1,6 @@
 "use client";
-import { Logo } from "@/components/Logo";
-import { Card } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { FC, useState } from "react";
-import { useComplexity } from "./complexity";
-import { ArrowRight, ArrowUp, ArrowUpIcon, LoaderCircle } from "lucide-react";
-import { Button } from "./ui/button";
-import { AnswerStep } from "./AnswerStep";
-import { cn } from "@/lib/utils";
 import { Session } from "./Session";
 import { Start } from "./Start";
-
-const examples = [
-  "xz backdoor",
-  "EU AltStore coming soon",
-  "Altman gives up OpenAI fund",
-  "Microplastics found in ancient digs",
-  "US House bans use of Microsoft Copilot",
-];
 
 export function MainContent() {
   return (
