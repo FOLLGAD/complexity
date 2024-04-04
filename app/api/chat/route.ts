421e8c7e399e5d109f285d079af4cb394b0c6d26
Hamed Mohammadpour <HamedMP@users.noreply.github.com>
2024-04-04 11:44:04 +0200

    Update the cohere model to be read from .env, otherwise use 'command' as default value

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,33 +1,33 @@
-import { CohereClient } from "cohere-ai";
+import { CohereClient } from 'cohere-ai';
 
 const cohere = new CohereClient({
   token: process.env.COHERE_API_KEY,
 });
 
-export const dynamic = "force-dynamic";
-export const runtime = "edge";
+export const dynamic = 'force-dynamic';
+export const runtime = 'edge';
 
 export async function POST(request: Request) {
   const { message, history } = await request.json();
 
   const stream = await cohere.chatStream({
-    model: "command-r",
+    model: process.env.COHERE_MODEL || 'command',
     message: message,
     preamble:
-      "You are Perplexity, an AI search LLM. User will input queries, you will try to inform the user about the query as well as possible. Keep it a bit brief but very informative." +
-      "\n\n" +
+      'You are Perplexity, an AI search LLM. User will input queries, you will try to inform the user about the query as well as possible. Keep it a bit brief but very informative.' +
+      '\n\n' +
       `Current time: ${new Date().toISOString()}`,
     chatHistory: history,
     temperature: 0.1,
-    promptTruncation: "AUTO",
-    citationQuality: "fast",
+    promptTruncation: 'AUTO',
+    citationQuality: 'fast',
     connectors: [
       {
-        id: "web-search",
+        id: 'web-search',
         // @ts-ignore
-        name: "Web Search",
-        created_at: "0001-01-01T00:00:00Z",
-        updated_at: "0001-01-01T00:00:00Z",
+        name: 'Web Search',
+        created_at: '0001-01-01T00:00:00Z',
+        updated_at: '0001-01-01T00:00:00Z',
         active: true,
         continue_on_failure: true,
       },
@@ -42,7 +42,7 @@ export async function POST(request: Request) {
     async start(controller) {
       for await (const chat of stream) {
         console.log(`${randomSequence} – ${chat.eventType}`);
-        controller.enqueue(encoder.encode(JSON.stringify(chat) + "\n"));
+        controller.enqueue(encoder.encode(JSON.stringify(chat) + '\n'));
       }
       console.log(`${randomSequence} – CLOSE`);
       controller.close();
@@ -54,7 +54,7 @@ export async function POST(request: Request) {
 
   return new Response(customReadable, {
     headers: {
-      "Content-Type": "text/html; charset=utf-8",
+      'Content-Type': 'text/html; charset=utf-8',
     },
   });
 }
