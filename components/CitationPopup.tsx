e9a5861a55d635ac894c2b9b84dc45bb22a1fca2
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-04-06 12:37:34 +0200

    g

--- components/CitationPopup.tsx
+++ components/CitationPopup.tsx
@@ -1,9 +1,98 @@
-import { ReactNode } from "react";
+import {
+  FC,
+  MutableRefObject,
+  PropsWithRef,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Citation, Document } from "./AnswerStep";
 import { CitationCard } from "./CitationCard";
 import { TrackedLink } from "./TrackedLink";
-import { Tooltip, TooltipTrigger } from "./ui/tooltip";
-import { TooltipContent } from "@radix-ui/react-tooltip";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { cn } from "@/lib/utils";
+
+const useScrollProgress = (
+  scrollRef: MutableRefObject<HTMLDivElement>,
+  dir: "x" | "y" = "y",
+) => {
+  const [scrollProgress, setScrollProgress] = useState({
+    scrolled: 0,
+    remaining: 0,
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!scrollRef || !scrollRef.current) return;
+
+      let remaining = 0,
+        scrolled = 0;
+
+      if (dir === "x") {
+        scrolled = scrollRef.current.scrollLeft;
+        remaining =
+          scrollRef.current.scrollWidth -
+          scrollRef.current.clientWidth -
+          scrolled;
+      } else {
+        scrolled = scrollRef.current.scrollTop;
+        remaining =
+          scrollRef.current.scrollHeight -
+          scrollRef.current.clientHeight -
+          scrolled;
+      }
+
+      setScrollProgress({
+        scrolled,
+        remaining,
+      });
+    };
+    scrollRef.current.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () =>
+      scrollRef?.current?.removeEventListener("scroll", handleScroll);
+  }, [dir]);
+
+  return scrollProgress;
+};
+
+const DocumentsContent: FC<
+  PropsWithRef<{ documents: Document[]; ref?: React.Ref<HTMLDivElement> }>
+> = ({ documents }: { documents: Document[] }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { remaining } = useScrollProgress(scrollRef, "x");
+
+  return (
+    <div
+      className="flex flex-row gap-2 overflow-x-auto rounded-lg bg-card/95 shadow-xl"
+      ref={scrollRef}
+    >
+      {documents.map((doc) => (
+        <TrackedLink
+          href={doc.url}
+          key={doc.id}
+          target="_blank"
+          rel="noreferrer"
+          phData={{
+            url: doc.url,
+          }}
+        >
+          <CitationCard key={doc.url} citation={doc} className="shadow-lg" />
+        </TrackedLink>
+      ))}
+      <div
+        className={cn(
+          "absolute bottom-0 right-0 top-0 h-full w-32 rounded-lg bg-gradient-to-r from-transparent to-black opacity-0 transition-opacity",
+          {
+            "opacity-50": remaining > 5,
+            "group-hover:opacity-25": remaining > 5,
+          },
+        )}
+      ></div>
+    </div>
+  );
+};
 
 export const CitationPopup = ({
   citation,
@@ -21,37 +110,19 @@ export const CitationPopup = ({
 
   return (
     <span className="relative">
-      <Tooltip delayDuration={500}>
-        <TooltipTrigger className="inline" asChild>
-          <span className="cursor-pointer text-orange-400 text-opacity-90">
+      <Popover>
+        <PopoverTrigger className="inline" asChild>
+          <span className="citation-mark cursor-pointer rounded bg-orange-400/50 transition-colors hover:bg-orange-400/75 focus:bg-orange-400/75">
             {children}
           </span>
-        </TooltipTrigger>
-        <TooltipContent className="z-20" key={id}>
-          <div className="not-prose group relative w-[min(500px,_100svw)] max-w-[max-content] rounded-lg">
-            <div className="flex flex-row gap-2 overflow-x-auto rounded-lg bg-card/95 p-2 shadow-xl">
-              {docs.map((doc) => (
-                <TrackedLink
-                  href={doc.url}
-                  key={doc.id}
-                  target="_blank"
-                  rel="noreferrer"
-                  phData={{
-                    url: doc.url,
-                  }}
-                >
-                  <CitationCard
-                    key={doc.url}
-                    citation={doc}
-                    className="shadow-lg"
-                  />
-                </TrackedLink>
-              ))}
-              <div className="absolute bottom-0 right-0 top-0 h-full w-32 rounded-lg bg-gradient-to-r from-transparent to-black opacity-50 transition-opacity group-hover:opacity-10"></div>
-            </div>
-          </div>
-        </TooltipContent>
-      </Tooltip>
+        </PopoverTrigger>
+        <PopoverContent
+          className="not-prose group relative z-20 w-[min(500px,_100svw)] max-w-[max-content] rounded-lg px-2 py-2"
+          key={id}
+        >
+          <DocumentsContent documents={docs} key={id} />
+        </PopoverContent>
+      </Popover>
     </span>
   );
 };
