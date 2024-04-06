71abe061e216044c2295eb355e830654c20f9695
Hamed Mohammadpour <HamedMP@users.noreply.github.com>
2024-04-06 21:38:28 +0200

    Customize Sonner component icons

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group border-0"
       toastOptions={{
         classNames: {
           toast:
@@ -21,6 +21,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          icon: "group-data-[type=error]:text-red-500 group-data-[type=success]:text-green-500 group-data-[type=info]:text-orange-500 group-data-[type=warning]:text-amber-500 group-data-[type=info]:text-blue-500",
         },
       }}
       {...props}
