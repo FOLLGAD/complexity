ed8d67a407de60fb7d4ead1b595c9505dfc63268
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-04-09 16:57:06 +0200

    add tooltip

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -23,7 +23,7 @@ import {
 } from "@/components//ui/card";
 import { Input } from "@/components//ui/input";
 import { toast } from "sonner";
-import { Tooltip } from "./ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export const Feedback = forwardRef<
   HTMLDivElement,
@@ -108,39 +108,51 @@ function ShareButton({ sessionId }: { sessionId: string }) {
   }, []);
 
   return (
-    <Button
-      variant="outline"
-      title="Share answer with others"
-      className={cn("feedback-button group", "hover:bg-zinc-300")}
-      onClick={handleCopy}
-    >
-      <Popover open={shareOpen} onOpenChange={setShareOpen}>
-        <PopoverTrigger>
-          <Share2Icon className="feedback-icon group-hover:text-gray-500" />
-        </PopoverTrigger>
-        <PopoverContent
-          className="relative z-20 w-[min(500px,_100svw)] max-w-fit rounded-lg bg-card p-0"
-          side="top"
+    <Tooltip delayDuration={100}>
+      <TooltipTrigger asChild>
+        <Button
+          variant="outline"
+          title="Share answer with others"
+          className={cn("feedback-button group", "hover:bg-zinc-300")}
+          onClick={handleCopy}
         >
-          <Card className="">
-            <CardHeader>
-              <CardTitle>Share your findings with others:</CardTitle>
-            </CardHeader>
-            <CardContent className="flex w-full justify-center gap-2">
-              <Input
-                autoFocus
-                value={url}
-                readOnly
-                className="inline-block w-fit cursor-text"
-              ></Input>
-              <Button variant="default" className="group" onClick={handleCopy}>
-                <Copy className="h-4 w-4" />
-              </Button>
-            </CardContent>
-            <div className="mt-2 flex justify-center gap-2"></div>
-          </Card>
-        </PopoverContent>
-      </Popover>
-    </Button>
+          <Popover open={shareOpen} onOpenChange={setShareOpen}>
+            <PopoverTrigger>
+              <Share2Icon className="feedback-icon group-hover:text-gray-500" />
+            </PopoverTrigger>
+            <PopoverContent
+              className="relative z-20 w-[min(500px,_100svw)] max-w-fit rounded-lg bg-card p-0"
+              side="top"
+            >
+              <Card className="">
+                <CardHeader>
+                  <CardTitle>Share your findings with others:</CardTitle>
+                </CardHeader>
+                <CardContent className="flex w-full justify-center gap-2">
+                  <Input
+                    autoFocus
+                    value={url}
+                    readOnly
+                    className="inline-block w-fit cursor-text"
+                  ></Input>
+                  <Button
+                    variant="default"
+                    className="group"
+                    onClick={handleCopy}
+                  >
+                    <Copy className="h-4 w-4" />
+                  </Button>
+                </CardContent>
+                <div className="mt-2 flex justify-center gap-2"></div>
+              </Card>
+            </PopoverContent>
+          </Popover>
+        </Button>
+      </TooltipTrigger>
+
+      <TooltipContent className="bg-primary/10 text-white">
+        <p>Share with others</p>
+      </TooltipContent>
+    </Tooltip>
   );
 }
